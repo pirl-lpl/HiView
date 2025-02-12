f5a101c92ca9b3620dce5ba9a9649bd40ea04d7f
GuyMac <guymac@gmail.com>
2025-02-12 10:23:31 -0700

    change include path to match installed dependency

--- PDS_Metadata.hh
+++ PDS_Metadata.hh
@@ -33,7 +33,7 @@ Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 class QNetworkAccessManager;
 class QNetworkReply;
 
-#include	"Parameter.hh"
+#include	"PVL/Parameter.hh"
 namespace idaeim {
 namespace PVL {
 class Lister;
