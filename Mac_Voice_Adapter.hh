800eafd238274ea21f1874c36401d75f40dd9da0
Guy McArthur <guym@arizona.edu>
2025-02-28 11:12:53 -0700

    cleanup

--- Mac_Voice_Adapter.hh
+++ Mac_Voice_Adapter.hh
@@ -20,37 +20,30 @@ along with this library; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 
 *******************************************************************************/
-
-#ifndef HiView_Mac_Voice_Adapter_hh
-#define HiView_Mac_Voice_Adapter_hh
+#pragma once
 
 #include "Image_Viewer.hh"
 #include "Statistics_Tool.hh"
 #include "Data_Mapper_Tool.hh"
-using namespace UA::HiRISE;
+using UA::HiRISE::Image_Viewer, UA::HiRISE::Statistics_Tools, UA::HiRISE::Data_Mapper_Tool;
 
 #include "Voice_Adapter.hh"
 #include "SpeechHandler.hh"
 
 class Mac_Voice_Adapter : Voice_Adapter
 {
-    
-public:    
-/*==============================================================================
-	Constants
-*/
-//!	Class identification name with source code version and date.
-static const char* const ID;
-	
-    Mac_Voice_Adapter(UA::HiRISE::Image_Viewer* viewer, UA::HiRISE::Statistics_Tools* stattool, UA::HiRISE::Data_Mapper_Tool* mapper);
+ public:
+    /*==============================================================================
+        Constants
+    */
 
+    //! Class identification name with source code version and date.
+    static const char *const ID;
+
+    Mac_Voice_Adapter(Image_Viewer *viewer, Statistics_Tools *stattool, Data_Mapper_Tool *mapper);
 
     void toggle(bool on);
-    
-private:
-    SpeechHandler* speechHandler;
-    
+
+ private:
+    SpeechHandler *speechHandler;
 };
-
-
-#endif /* HiView_Mac_Voice_Adapter_hh */
