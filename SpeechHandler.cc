13ead356d7d29f6e00efc343112cbb5041640b81
Guy McArthur <guym@arizona.edu>
2025-03-26 16:55:59 -0700

    code update

--- SpeechHandler.cc
+++ SpeechHandler.cc
@@ -1,11 +1,29 @@
+/*	SpeechHandler.cc
 
+HiROC CVS ID: $Id$
+
+Copyright (C) 2009-2025  Arizona Board of Regents on behalf of the
+Planetary Image Research Laboratory, Lunar and Planetary Laboratory at
+the University of Arizona.
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License, version 2.1,
+as published by the Free Software Foundation.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this library; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+
+*/
 #include "SpeechHandler.hh"
-#include "Voice_Adapter.hh"
 
-SpeechHandler::SpeechHandler(Voice_Adapter *adapter) : voice_adapter(adapter)
-{
-}
+SpeechHandler::SpeechHandler(Voice_Adapter* adapter) : voice_adapter(adapter)
+{}
 
 SpeechHandler::~SpeechHandler()
-{
-}
+{}
